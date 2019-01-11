33c4c539d6dc5705baa6adff9deb747e4b62348c
Chaman Gurjar <chamangujjar162@gmail.com>
2019-01-11 21:48:25 +0530

    Initial Commit

---
+++ Tinder-Clone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tinder-Clone
+//
+//  Created by Chaman Gurjar on 11/01/19.
+//  Copyright © 2019 Chaman Gurjar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
