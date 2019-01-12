46e8400ab84b28a7de3c03f1575d3c0eaad002f4
Chaman Gurjar <chamangujjar162@gmail.com>
2019-01-12 10:14:07 +0530

    UILabel (Element) moved right and left on draging. PanGestureRecognizer for dragging is added.

--- Tinder-Clone/ViewController.swift
+++ Tinder-Clone/ViewController.swift
@@ -11,23 +11,36 @@ import Parse
 
 //Parse Server Setup/Installed At :- https://www.back4app.com
 class ViewController: UIViewController {
+    @IBOutlet weak var swipeLabel: UILabel!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        saveCustomObject()
+        setupDraggingGesture()
     }
     
-    private func saveCustomObject() {
-        let customTestObect = PFObject(className: "CustomTestObect")
-        customTestObect["checking"] = "isParseConnected"
-        customTestObect.saveInBackground { (success, error) in
-            if success {
-                print("Hurrah, Successfully conected to parse server and saved object. Cheers")
+    private func setupDraggingGesture() {
+        //  Pan Gesture is used to drag element Left or Right.
+        let swipeGesture = UIPanGestureRecognizer(target: self, action: #selector(dragElementLeftOrRight(gestureRecognizer:)))
+        swipeLabel.addGestureRecognizer(swipeGesture)
+    }
+    
+    @objc private func dragElementLeftOrRight(gestureRecognizer: UIPanGestureRecognizer) {
+        let labelPoint = gestureRecognizer.translation(in: view)
+        swipeLabel.center = CGPoint(x: view.bounds.width / 2 + labelPoint.x, y: view.bounds.height / 2 + labelPoint.y)
+        
+        if gestureRecognizer.state == .ended {
+            if swipeLabel.center.x < (view.bounds.width / 2 - 100) {
+                print("Not Interested")
+            } else if swipeLabel.center.x > (view.bounds.width / 2 + 100) {
+                print("Interested")
             }
+            
+            swipeLabel.center = CGPoint(x: view.bounds.width / 2, y: view.bounds.height / 2)
         }
     }
+    
     
 }
 
