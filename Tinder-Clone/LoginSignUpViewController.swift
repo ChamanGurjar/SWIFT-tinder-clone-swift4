9c6d8fe0b8c9ce47ddd0cfb3a171effc79b17030
Chaman Gurjar <chamangujjar162@gmail.com>
2019-01-12 11:39:20 +0530

    LoginSignUpUIAdded

---
+++ Tinder-Clone/LoginSignUpViewController.swift
@@ -0,0 +1,31 @@
+//
+//  LoginSignUpViewController.swift
+//  Tinder-Clone
+//
+//  Created by Chaman Gurjar on 12/01/19.
+//  Copyright © 2019 Chaman Gurjar. All rights reserved.
+//
+
+import UIKit
+
+class LoginSignUpViewController: UIViewController {
+    
+    @IBOutlet weak var usernameTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    @IBOutlet weak var loginSignUpButton: UIButton!
+    @IBOutlet weak var changeLoginSignUpModeButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    @IBAction func loginSignUpUser(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func changeLoginSignUpMode(_ sender: UIButton) {
+        
+    }
+}
