11abed40af92c7cac7679e56d8c9c1035bef972d
Chaman Gurjar <chamangujjar162@gmail.com>
2019-01-14 11:20:30 +0530

    MatchViewController added.

---
+++ Tinder-Clone/MatchesViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MatchesViewController.swift
+//  Tinder-Clone
+//
+//  Created by Chaman Gurjar on 14/01/19.
+//  Copyright © 2019 Chaman Gurjar. All rights reserved.
+//
+
+import UIKit
+
+class MatchesViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    @IBAction func back(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+}
